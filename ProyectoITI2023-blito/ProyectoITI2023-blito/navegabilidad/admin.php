a19fdf387591940eb619f2e7dfcc677b4c13bad1
blitocerro <134820391+blitocerro@users.noreply.github.com>
2023-08-08 12:13:55 -0300

    Add files via upload
    me quiero matar no anda bien las validaciones con los foreach, posiblemente tenga que cambiar la mayoria de la logica

---
+++ ProyectoITI2023-blito/ProyectoITI2023-blito/navegabilidad/admin.php
@@ -0,0 +1,29 @@
+<?php
+// PROTEGER LA PAGINA SIN ANTES INICIAR SESSION//
+session_start();
+if(!isset($_SESSION['admin'])){
+    echo '
+    <script>
+       alert("Por favor debes iniciar session");
+       window.location = "../index.html";
+    </script>
+
+    ';
+    session_destroy();
+    die();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ADMINISTRADOR</h1>
+    <a href="cerrar_session.php">Cerrar Session</a>
+</body>
+</html>
