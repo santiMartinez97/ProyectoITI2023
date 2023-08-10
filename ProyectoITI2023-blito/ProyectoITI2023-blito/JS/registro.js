4876d5cf192544fda5bff0862c7039b8662058e3
blitocerro <134820391+blitocerro@users.noreply.github.com>
2023-08-10 13:49:29 -0300

    registro casi terminado

--- ProyectoITI2023-blito/ProyectoITI2023-blito/JS/registro.js
+++ ProyectoITI2023-blito/ProyectoITI2023-blito/JS/registro.js
@@ -2,181 +2,84 @@
 var formulario = document.getElementById("formulario");
 var respues = document.getElementById("respuesta");
 
+//datos ingresados del html cliente web
 const inputs = document.querySelectorAll('#formulario input');
 
-//datos ingresados del html cliente web
+var botonId = document.getElementById("botonAlerta")
 
-var nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var ci = document.getElementById("ci");
-var email = document.getElementById("email");
-var password = document.getElementById("pass");
-var telefono = document.getElementById("telefono");
-var calle = document.getElementById("calle");
-var numero = document.getElementById("numero");
-var esquina = document.getElementById("esquina");
-var barrio = document.getElementById("barrio");
+const expresionesRegulares = {
+  email : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  nombre : /^[a-zA-ZÀ-ÿ\s]{2,40}$/, // Letras y espacios con acentos.
+  apellido : /^[a-zA-ZÀ-ÿ\s]{2,40}$/, //letras y espacion con acentos
+  ci : /^\d{8}(\.\d+)?$/, // 8 digitos
+  password : /^.{6,17}$/, // 6 a 17 digitos.
+  telefono : /^09\d{7}$/, // 9 numeros.
+  calle : /^[a-zA-ZÀ-ÿ\s]{2,30}$/, //\d{2}
+  numero : /^\d{3,4}(\.\d+)?$/, //numero casa
+  esquina : /^[a-zA-ZÀ-ÿ\s]{2,30}$/,
+  barrio : /^[a-zA-ZÀ-ÿ\s]{2,30}$/
+}
 
-//span de los warnings
-var parrafo = document.getElementById("warnings");
-var parrafoApellido = document.getElementById("warningsApellido");
-var parrafoCi = document.getElementById("warningsCi");
-var parrafoEmail = document.getElementById("warningsEmail");
-var parrafoPassword = document.getElementById("warningsPassword");
-var parrafoTelefono = document.getElementById("warningsTelefono");
-var parrafoCalle = document.getElementById("warningsCalle");
-var parrafoNumero = document.getElementById("warningsNumero");
-var parrafoEsquina = document.getElementById("warningsEsquina");
-var parrafoBarrio = document.getElementById("warningsBarrio");
-
-var parrafoBoton = document.getElementById("warningsBoton");
+const validacionCampos = {
+  email : false,
+  nombre : false,
+  apellido : false,
+  ci : false,
+  password : false,
+  telefono : false, 
+  calle : false,
+  numero : false,
+  esquina : false,
+  barrio : false
+}
 
 const validarFormulario = (e) => {
-	var confirmar = false;
-  var warnings = "";
-  var warningsApellido = "";
-  var warningsCi = "";
-  var warningsEmail = "";
-  var warningsPassword = "";
-  var warningsTelefono = "";
-  var warningsCalle = ""; 
-  var warningsNumero = "";
-  var warningsEsquina = "";
-  var warningsBarrio = "";
-//expresiones regulares  
-  var confirmarEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  var confirmarNombre = /^[a-zA-ZÀ-ÿ\s]{2,40}$/; // Letras y espacios con acentos.
-  var confirmarApellido = /^[a-zA-ZÀ-ÿ\s]{2,40}$/; //letras y espacion con acentos
-	var confirmarCi =  /^.{8}$/; // 8 digitos
-  var confirmarPassword = /^.{6,17}$/; // 6 a 17 digitos.
-	var confirmarEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/; //valida email
-	var confirmarTelefono = /^09\d{7}$/; // 9 numeros.
-  var confimrarCalle = /^[a-zA-ZÀ-ÿ\s]{2,30}$/; //\d{2}
-  var confirmarNumero = /^.{3,4}$/; //numero casa
-  var confirmarEsquina = /^[a-zA-ZÀ-ÿ\s]{2,30}$/;
-  var confirmarBarrio = /^[a-zA-ZÀ-ÿ\s]{2,30}$/;
-  
-//validacion de datos web 
-  if(!confirmarNombre.test(nombre.value)){
-    warnings += 'El nombre no es valido';
-    confirmar = true;  
-    nombre.classList.add("error"); 
-    }
-  else{
-    nombre.classList.add("correcto");
-    nombre.classList.remove("error");
+  switch (e.target.name) {
+    case "nombre":
+      validarCampos(expresionesRegulares.nombre, e.target, 'nombre');
+    break;
+    case "apellido":
+      validarCampos(expresionesRegulares.apellido, e.target, 'apellido');      
+    break;
+    case "ci":
+      validarCampos(expresionesRegulares.ci, e.target, 'ci');
+    break;
+    case "email":
+      validarCampos(expresionesRegulares.email, e.target, 'email');
+    break;
+    case "password":
+      validarCampos(expresionesRegulares.password, e.target, 'password');
+    break;
+    case "telefono":
+      validarCampos(expresionesRegulares.telefono, e.target, 'telefono');
+    break;
+    case "calle":
+      validarCampos(expresionesRegulares.calle, e.target, 'calle');   
+    break;
+    case "numero":
+      validarCampos(expresionesRegulares.numero, e.target, 'numero');//valida
+    break;
+    case "esquina":
+      validarCampos(expresionesRegulares.esquina, e.target, 'esquina');//valida
+    break;
+    case "barrio":
+      validarCampos(expresionesRegulares.barrio, e.target, 'barrio');//valida
+    break;
   }
+}
 
-  if(!confirmarApellido.test(apellido.value)){
-    warningsApellido += 'El apellido no es valido ';
-    confirmar = true;
-    apellido.classList.add("error");
+const validarCampos = (expresion, input, campo) => {
+  if (expresion.test(input.value)) {
+    document.getElementById(`grupo__${campo}`).classList.remove("grupo__error");
+    document.getElementById(`grupo__${campo}`).classList.add("grupo__correcto");
+    document.querySelector(`#grupo__${campo} .grupo_input-error`).classList.remove('grupo_input-error-activo');
+    validacionCampos[campo] = true;
   }
   else {
-    apellido.classList.add("correcto");
-    apellido.classList.remove("error");  
-  }
-
-  if(parseInt(ci.value) < 0 || !confirmarCi.test(ci.value)){
-    warningsCi += 'La cedula de identidad no es valida ';
-    confirmar = true;
-    ci.classList.add("error");
-  }
-  else{
-    ci.classList.add("correcto");
-    ci.classList.remove("error");
-  }
-
-  if(!confirmarEmail.test(email.value)){
-    warningsEmail += 'El email no es valido ';
-    confirmar = true;
-    email.classList.add("error");
-  }
-  else{
-    email.classList.add("correcto");
-    email.classList.remove("error");
-  }
-
-  if(!confirmarPassword.test(password.value)){
-    warningsPassword += 'La contrasena es poca segura';
-    confirmar = true;
-    password.classList.add("error");
-  }
-  else {
-    password.classList.add("correcto");
-    password.classList.remove("error");
-  }
-  if(parseInt(telefono.value) < 0 || !confirmarTelefono.test(telefono.value)){
-    warningsTelefono += 'El telefono no es valido';
-    confirmar = true;
-    telefono.classList.add("error");
-  }
-  else{
-    telefono.classList.add("correcto");
-    telefono.classList.remove("error");
-  }
-  if(!confimrarCalle.test(calle.value)){
-    warningsCalle += 'La calle no es valida';
-    confirmar = true;
-    calle.classList.add("error");
-  }
-  else{
-    calle.classList.add("correcto");
-    calle.classList.remove("error");
-  }
-
-  if(parseInt(numero.value) < 0 || !confirmarNumero.test(numero.value)){
-    warningsNumero += 'El numero de casa no es valido';
-    confirmar = true;
-    numero.classList.add("error");
-  }
-  else{
-    numero.classList.add("correcto");
-    numero.classList.remove("error");
-  }
-
-  if(!confirmarEsquina.test(esquina.value)){
-    warningsEsquina += 'La esquina no es valida';
-    confirmar = true;
-    esquina.classList.add("error");
-  }
-  else {
-    esquina.classList.add("correcto");
-    esquina.classList.remove("error");
-  }
-  if(!confirmarBarrio.test(barrio.value)){
-    warningsBarrio += 'El barrio no es valido ';
-    confirmar = true;
-    barrio.classList.add("error");
-  }
-  else {
-    barrio.classList.add("correcto");
-    barrio.classList.remove("error");
-  }
-  if (confirmar) {
-    parrafo.innerHTML = warnings;
-    parrafoApellido.innerHTML = warningsApellido;
-    parrafoCi.innerHTML = warningsCi;
-    parrafoEmail.innerHTML = warningsEmail;
-    parrafoPassword.innerHTML = warningsPassword;
-    parrafoTelefono.innerHTML = warningsTelefono;
-    parrafoCalle.innerHTML = warningsCalle;
-    parrafoNumero.innerHTML = warningsNumero;
-    parrafoEsquina.innerHTML = warningsEsquina;
-    parrafoBarrio.innerHTML = warningsBarrio;
-  }
-  else{
-    confirmar = false;  
-    parrafo.innerHTML = "";
-    parrafoApellido.innerHTML = "";
-    parrafoCi.innerHTML = "";
-    parrafoEmail.innerHTML = "";
-    parrafoPassword.innerHTML = "";
-    parrafoTelefono.innerHTML = "";
-    parrafoCalle.innerHTML = "";
-    parrafoNumero.innerHTML = "";
-    parrafoEsquina.innerHTML = "";
-    parrafoBarrio.innerHTML = "";
+    document.getElementById(`grupo__${campo}`).classList.remove("grupo__correcto");
+    document.getElementById(`grupo__${campo}`).classList.add("grupo__error");
+    document.querySelector(`#grupo__${campo} .grupo_input-error`).classList.add('grupo_input-error-activo');
+    validacionCampos[campo] = false;
   }
 }
 
@@ -187,24 +90,21 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
   e.preventDefault(); // Evitar que se ejecute lo que viene por defecto en el navegador.
-
-  if (!validarFormulario.confirmar) {
-    warningsBoton='';
-    warningsBoton += 'Porfavor complete bien los campos';
-    parrafoBoton.innerHTML = warningsBoton;
-  }
-  else{
-    var datos = new FormData(formulario); 
+if (validacionCampos.nombre && validacionCampos.apellido && validacionCampos.ci && validacionCampos.email && validacionCampos.telefono && validacionCampos.password && validacionCampos.calle && validacionCampos.numero && validacionCampos.esquina && validacionCampos.barrio){
+  var datos = new FormData(formulario); 
     let url = 'BACKPHP/registroClienteWeb.php';
-
+    botonId.classList.remove("grupo_input-error-activo");
   fetch(url, {
     method: "POST",
     body: datos,
   
   });
   
-  document.getElementById("formulario").reset();
+  formulario.reset();
   alerta();
+}
+else {
+  botonId.classList.add("grupo_input-error-activo");
 }
 });
 
