299ad271dd8bc55bdbc49f3c94e67c19014edb75
santiMartinez97 <santiagomartinez210497@gmail.com>
2023-08-26 20:21:56 -0300

    Login - cuarta versión
    Agregado estilo a los mensajes de error.

--- JS/login.js
+++ JS/login.js
@@ -1,5 +1,12 @@
 var formulario = document.getElementById("loginForm");
 
+let datosIncorrectos = `<article class="alert alert-danger d-flex align-items-center" role="alert">
+<i class="fa-solid fa-circle-exclamation"></i> Usuario y/o contraseña incorrecto.</article>`;
+let limiteExcedido = `<article class="alert alert-danger d-flex align-items-center" role="alert">
+<i class="fa-solid fa-lock"></i> Limite de intentos excedido. Intente más tarde.</article>`;
+let usuarioNoHabilitado = `<article class="alert alert-danger d-flex align-items-center" role="alert">
+<i class="fa-solid fa-user-slash"></i> El usuario no se encuentra habilitado.</article>`;
+
 formulario.addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -40,18 +47,18 @@ formulario.addEventListener("submit", function (e) {
 
         case "No habilitado":
           document.getElementById("loginForm").reset();
-          alert("El usuario no se encuentra habilitado.");
+          document.getElementById("loginError").innerHTML = usuarioNoHabilitado;
           break;
 
         case "Bloqueado":
           document.getElementById("loginForm").reset();
           document.getElementById("enviar").disabled = true;
-          alert("Limite de intentos excedido. Intente más tarde.");
+          document.getElementById("loginError").innerHTML = limiteExcedido;
           break;
 
         default:
           document.getElementById("loginForm").reset();
-          alert("Usuario y/o Contraseñas incorrectos.");
+          document.getElementById("loginError").innerHTML = datosIncorrectos;
       }
 
     })
