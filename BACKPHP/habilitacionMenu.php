a1ce38a6535604e6320970fa90e0d15c0c9cdebd
Ignacio Lopez <ignacio8356@gmail.com>
2023-09-06 00:15:56 -0300

    BajaMenu,Habilitar y deshabilitar y publicar menu por filtro de dieta

---
+++ BACKPHP/habilitacionMenu.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../config/config.php';
+require '../config/conexion.php';
+
+$db = new DataBase();
+$con = $db->conectar();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $menuID = $_POST['menuID'];
+    $menuStatus = $_POST['menuStatus'];
+
+    if($menuStatus == "true"){
+        $menu = $con->prepare("UPDATE menu SET Habilitacion = 'Habilitado' WHERE ID = ?");
+        $menu-> execute([$menuID]);
+    }else{
+        $menu = $con->prepare("UPDATE menu SET Habilitacion = 'No habilitado' WHERE ID = ?");
+        $menu-> execute([$menuID]);
+    }
+
+    // Si la actualización se realiza con éxito, devuelve 'success'
+    echo 'success';
+} else {
+    echo 'Error: Método no válido.';
+}
+?>
---
+++ navegabilidad/gerenteBaja.php
@@ -0,0 +1,93 @@
+<?php
+
+require '../config/conexion.php';
+
+$db = new DataBase();
+$con = $db->conectar();
+
+$menu = $con->prepare("SELECT *  FROM menu ");
+$menu-> execute();
+$resultado = $menu->fetchAll(PDO::FETCH_ASSOC);
+
+$menu_array=[];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jefe de cocina | NutriBento</title>
+    <link rel="stylesheet" href="../CSS/jefeDeCocina.css" />
+</head>
+<body>
+
+     <h1>Control de Menu</h1>
+
+       
+        <a href="gerente.php">Alta</a>
+        <a href="cerrar_session.php">Cerrar Session</a>
+
+
+    <article class="pedidos">
+    <table>
+            <thead>
+                <tr>
+                    <th class="tablaArriba">Periocidad</th>
+                    <th class="tablaArriba">Menu</th>
+                    <th class="tablaArriba">Precio</th>
+                    <th class="tablaArriba">Descuento</th>
+                    <th class="tablaArriba">Stock</th>
+                    <th class="tablaArriba">Stock minimo</th>
+                    <th class="tablaArriba">Stock maximo</th>
+                    <th class="tablaArriba">Habilitacion</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($resultado as $row) {
+                    $Periocidad = $row['Periodicidad'];
+                    $menu = $row['Nombre'];
+                    $precio = $row['Precio']; 
+                    $descuento = $row['Descuento']; 
+                    $stockActual = $row['Stock'];
+                    $stockMinimo = $row['StockMinimo'];
+                    $stockMaximo = $row['StockMaximo'];
+
+                    if (!in_array($menu, $menu_array)) {      
+                        echo '<tr data-client-id="'.$row['ID'].'">';
+                        echo '<th >'.$Periocidad.'</th> ';
+                        echo '<th >'.$menu.'</th> ';
+                       
+                        echo '<th >'.$precio.'</th> ';
+                        echo '<th >'.$descuento.'</th> ';
+                        echo '<th >'.$stockActual.'</th> ';    
+                        echo '<th >'.$stockMinimo.'</th> ';    
+                        echo '<th >'.$stockMaximo.'</th> ';    
+                        
+
+                        if($row['Habilitacion'] === "No habilitado"){
+                            echo '<td data-client-status="false">'.$row['Habilitacion'].'</td>';
+                            echo '<td><button class="botonAceptar habilitar-btn">Habilitar</button></td>';
+                            echo '<td><button class="botonDesechar">Eliminar</button></td>';
+                        }else{
+                           echo '<td data-client-status="true">'.$row['Habilitacion'].'</td>';
+                            echo '<td><button class="botonRechazar habilitar-btn">Deshabilitar</button></td>';
+                            echo '<td><button class="botonDesechar">Eliminar</button></td>';
+                        }
+                        echo '</tr>';
+                    
+                     }
+                    }
+                  
+                    
+       
+                ?>
+                  
+            </tbody>
+        </table>
+        <script src="../JS/jquery-3.6.4.min.js"></script>
+        <script src="../JS/adminGerente.js"></script>
+        
