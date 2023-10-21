0ff07fd44c4a2dfe8ad15682a9bb01ee4bb365bb
Ignacio Lopez <ignacio8356@gmail.com>
2023-10-21 15:09:59 -0300

    agregando MercadoPago

--- captura.php
+++ captura.php
@@ -1,16 +1,20 @@
 <?php
 
+require '../config/config.php';
+require '../config/conexion.php';
+require '../vendor/autoload.php';
 
-require 'config/config.php';
+
+// CREAR UNA CONSULTA PARA NOMBRE DE PERFIL
+$cliente = $con->prepare("SELECT ID FROM cliente ");
+$cliente-> execute();
+$cliente1 = $cliente->fetchAll(PDO::FETCH_ASSOC);
 
 $payment = $_GET['payment_id'];
 $status = $_GET['status'];
 $payment_type = $_GET['payment_type'];
 $order_id = $_GET['merchant_order_id'];
-
-
 echo "<h3>Pago exitoso </h3>";
-
 echo $payment. '<br>';
 echo $status. '<br>';
 echo $payment_type. '<br>';
@@ -19,9 +23,22 @@ echo $order_id. '<br>';
 //Se borra todas las cosas del carrito cuando se aprueba la compra
 unset($_SESSION['carrito']);
 
-//Falta agregar todo esto a la base de datos//
+// //Falta agregar todo esto a la base de datos//
+// $fecha_hoy = date("Y-m-d");
+// if ($cliente1 && isset($cliente1['ID'])) {
+//   $idCliente = $cliente1['ID'];
 
-?>
+//   $query = $con->prepare("INSERT INTO pedido (Fecha, IDCliente) VALUES (:fecha, :id_cliente)");
+//   $query->bindParam(':fecha', $fecha_hoy, PDO::PARAM_STR);
+//   $query->bindParam(':id_cliente', $idCliente, PDO::PARAM_INT);
+//   $query->execute();
+// } else {
+//   echo "No se encontró ningún cliente habilitado o el ID del cliente no está definido.";
+//   $idCliente = $cliente1['id'];
+//   echo $idCliente;
+// }  
+
+// ?>
 
 
 <!-- Podria mandarlo a una seccion "Mis compras y ver el estado" -->
